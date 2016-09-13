46f2c749ddddcdbefe254e7fae53bb825ae02d25
Luiz Guerra <lhguerra@gmail.com>
2016-09-13 11:40:33 -0300

    Updated packaging script to make a new Drupal build.

--- gulp/tasks/pack-distro.js
+++ gulp/tasks/pack-distro.js
@@ -5,5 +5,8 @@ var cwd = process.cwd();
 var repo = 'https://github.com/revagomes/pece-distro.git';
 
 gulp.task('pack-distro', function () {
+  shell.exec('rm -r distro');
   shell.exec('git clone ' + repo + ' distro');
+  shell.exec('drush kw-s');
+  shell.exec('drush kw-b');
 });
