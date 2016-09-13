6a932f72f9620ea253487aa9ee541e8f1c8f8007
Luiz Guerra <lhguerra@gmail.com>
2016-09-13 14:54:43 -0300

    Updated packaging script to commit new build.

--- gulp/tasks/pack-distro.js
+++ gulp/tasks/pack-distro.js
@@ -5,8 +5,18 @@ var cwd = process.cwd();
 var repo = 'https://github.com/revagomes/pece-distro.git';
 
 gulp.task('pack-distro', function () {
+  // Clone distro repository.
   shell.exec('rm -r distro');
   shell.exec('git clone ' + repo + ' distro');
+
+  // Build Drupal project.
   shell.exec('drush kw-s');
   shell.exec('drush kw-b');
+
+  // Update distro repository with new build.
+  shell.exec('cp -r build/* distro/');
+  shell.cd('distro');
+  shell.exec('pwd');
+  shell.exec('git add .');
+  shell.exec('git commit -m "Added new build."');
 });
