bb8f1b8e82833772160c5b2633f86ff79d0bf684
Luiz Guerra <lhguerra@gmail.com>
2016-09-13 10:46:45 -0300

    Fixed cloning commit in packaging task.

--- gulp/tasks/pack-distro.js
+++ gulp/tasks/pack-distro.js
@@ -5,6 +5,5 @@ var cwd = process.cwd();
 var repo = 'https://github.com/revagomes/pece-distro.git';
 
 gulp.task('pack-distro', function () {
-  shell.mkdir('/distro');
-  shell.exec('git clone' . repo . ' /distro');
+  shell.exec('git clone ' + repo + ' distro');
 });
