8d741d9d697397c6eb3891eba43eead47191efef
Renato Vasconcellos <revagomes@gmail.com>
2016-09-14 17:29:53 -0300

    Updated pack-distro task from gulp.

--- gulp/tasks/pack-distro.js
+++ gulp/tasks/pack-distro.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 
 var cwd = process.cwd();
 var repo = 'https://github.com/revagomes/pece-distro.git';
+var commitMessage = 'Added new release based on master of development repository.'
 
 gulp.task('pack-distro', function () {
   // Clone distro repository.
@@ -22,14 +23,10 @@ gulp.task('pack-distro', function () {
 var packDistroCallback = function () {
   shell.exec('git clone ' + repo + ' distro');
 
-  // Update distro repository with new build.
+  // Update distro repository with a new build.
   shell.exec('cp -a build/* distro/');
   shell.cd('distro');
   shell.exec('git add .');
-
-  var currentHead = shell.exec('git rev-parse HEAD').trim();
-  var commitMessage = 'Added new release based on master of development repository.'
-
   shell.exec(`git commit -m "${ commitMessage }"`);
   shell.exec(`git push ${repo} master`);
 }
